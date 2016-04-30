926fcace79967e1b50995decc9a070f8a5f3ccad
Juraj Hreško <juraj.hresko@firma.seznam.cz>
2016-04-30 18:12:01 +0200

    Add simple entry-point

--- src/clyamar/core.clj
+++ src/clyamar/core.clj
@@ -1,7 +1,14 @@
 (ns clyamar.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clyamar.lightsvm]
+            [clyamar.trees]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  (defn -main
+        "I don't do a whole lot ... yet."
+        [& args]
+        (println "Args:" args)
+        (let [[train-file] args
+              features (clyamar.lightsvm/read-features train-file)
+              labels (clyamar.lightsvm/read-labels train-file)
+              tholds (clyamar.trees/create-thresholds features 10)]
+          (println (count labels) (count (first features)) train-file)))
